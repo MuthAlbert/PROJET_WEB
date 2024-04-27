b053728590dfd899f99d6c22d9e40704e24237a3
Vatana <muth.albert69@gmail.com>
2024-04-28 00:28:17 +0200

    [ADD] optimisation of ticket request,working

--- user.js
+++ user.js
@@ -1,7 +1,6 @@
-function fermerPopup(event) {
-    event.preventDefault();
-    const popup = document.getElementById("popup");
-    popup.style.display = "none";
+function togglePopup() {
+    let popup = document.querySelector("#popup-overlay");
+    popup.classList.toggle("open");
 }
 
 function submitTicket() {
@@ -24,36 +23,8 @@ function submitTicket() {
         celltype.textContent = type;
         celldate.textContent = date;
         cellprix.textContent = prix;
+        togglePopup();
     } else {
         alert("Veuillez remplir tous les champs !");
     }
-}
-
-function display_ticket() {
-    const popup = document.getElementById("popup");
-    const popupDetails = document.getElementById("popup-details");
-    popupDetails.innerHTML = `
-    <div id="popup-details" class="container">
-    <h2 class="mb-4"><b>AJOUTER UN TICKET</b></h2>
-        <div class="mb-3">
-            <label for="choix" class="form-label">Types de frais*</label>
-            <select id="types" class="form-select">
-                <option value="RESTAURATION">RESTAURATION</option>
-                <option value="LOGEMENT">LOGEMENT</option>
-                <option value="TRANSPORT">TRANSPORT</option>
-            </select>
-        </div>
-        <div class="mb-3">
-            <label for="dateInput" class="form-label">Date*</label>
-            <input type="date" id="dateInput" class="form-control">
-        </div>
-        <div class="mb-3">
-            <label for="prixInput" class="form-label">Prix*</label>
-            <input type="number" id="prixInput" class="form-control">
-        </div>
-        <button onClick="submitTicket()" class="btn btn-primary mt-3 me-2">Soumettre</button>
-        <button onClick="fermerPopup()" class="btn btn-secondary mt-3">Annuler</button>
-    </div>
-    `;
-    popup.style.display = "block";
 }
