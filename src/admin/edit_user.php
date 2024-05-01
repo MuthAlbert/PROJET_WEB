9c7a9514346162c6851f94c7faa4adad27b40076
Sterennnnn <148686648+Sterennnnn@users.noreply.github.com>
2024-05-01 23:47:46 +0200

    Update edit_user.php

--- src/admin/edit_user.php
+++ src/admin/edit_user.php
@@ -5,16 +5,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Modifier utilisateur</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="style_admin.css">
 </head>
 <body>
+    <script  src="admin_js.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
-<div class="container">
+
+<div class="cadre1">
     <h1 class="text-center">Modifier utilisateur</h1>
     
 <?php
     if(isset($_GET['id']) 
-        && isset($_POST['prenom']) 
         && isset($_POST['nom']) 
+        && isset($_POST['prenom']) 
         && isset($_POST['mail']) 
         && isset($_POST['mot_de_passe']) 
         && isset($_POST['role'])) {
@@ -27,9 +31,9 @@
 
     $db = new PDO("mysql:host=localhost;dbname=torillec;charset=utf8mb4","root","");
     $stmt = $db->prepare("UPDATE utilisateur SET prenom = :prenom, nom = :nom, mail = :mail, mot_de_passe = :mot_de_passe, role = :role WHERE id_user = :id");
-
-    $stmt->bindParam(":prenom", $prenom);
+    
     $stmt->bindParam(":nom", $nom);
+    $stmt->bindParam(":prenom", $prenom);
     $stmt->bindParam(":mail", $mail);
     $stmt->bindParam(":mot_de_passe", $mot_de_passe);
     $stmt->bindParam(":role", $role);
@@ -40,9 +44,7 @@
     } else {
         echo "Une erreur s'est produite lors de la mise à jour des informations de l'utilisateur.";
     }
-} else {
-    echo "Veuillez remplir tous les champs du formulaire.";
-}
+} 
 ?>
     <?php
     if(isset($_GET['id'])) {
@@ -57,14 +59,14 @@
         if($user) {
             ?>
             <!-- Formulaire pour modifier l'utilisateur -->
-            <form action="edit_user.php?id=<?php echo $id; ?>" method="post">
+            <form style=""cadre1 action="edit_user.php?id=<?php echo $id; ?>" method="post">
                 <div class="mb-3">
-                    <label for="prenom" class="form-label">Prénom :</label>
-                    <input type="text" id="prenom" name="prenom" class="form-control" value="<?php echo $user['prenom']; ?>">
-                </div>
                 <div class="mb-3">
                     <label for="nom" class="form-label">Nom :</label>
                     <input type="text" id="nom" name="nom" class="form-control" value="<?php echo $user['nom']; ?>">
+                </div>
+                    <label for="prenom" class="form-label">Prénom :</label>
+                    <input type="text" id="prenom" name="prenom" class="form-control" value="<?php echo $user['prenom']; ?>">
                 </div>
                 <div class="mb-3">
                     <label for="mail" class="form-label">Mail :</label>
@@ -89,12 +91,12 @@
 
                     </select>
                 </div>
-                <button type="submit" class="btn btn-primary">Modifier</button>
-                <button type="" class="btn btn-primary"> <a href="admin.php">Retour</a></button>
+                <button type="submit" class="bouton">Modifier</button>
+                <button type="" class="bouton"> <a href="admin.php">Retour</a></button>
             </form>
             <?php
         } else {
-            echo "<p>Utilisateur non trouvé.</p>";
+            echo "<p>Utilisateur introuvable.</p>";
         }
     } else {
         echo "<p>Identifiant de l'utilisateur non spécifié.</p>";
