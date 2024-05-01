dadb9105648baad6d35db5fee0cd85dcb021b384
Vatana <91609492+MuthAlbert@users.noreply.github.com>
2024-05-01 20:00:58 +0200

    [UPDATE] user.php

--- src/user/user.php
+++ src/user/user.php
@@ -1,44 +1,94 @@
 <?php
 session_start();
-//Connexion a la db
-$host = '127.0.0.1';
-$db = 'torillec';
-$user ='root';
-$pass='';
-$charset='utf8mb4';
-
-$databaseDsn = "mysql:host=$host;dbname=$db;charset=$charset";
-
-$options = [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES => false,
-    ];
-try {
-    $connexion = new PDO($databaseDsn, $user, $pass, $options);
-    } catch (Exception $e) {
-    die('Erreur de connexion : ' . $e->getMessage());
-}
-
-//Récupération des information de l'utilisateur
-$id = $_SESSION['id'];
-
-//Echange avec la base de données
-$requete = "SELECT * FROM factures";
-$result = $bddPDO -> query($requete);
-
-if(!result){
-    echo "La récupération des données a échoué !";
-}
-if($id === $result['id']){
-    while($ligne = $result->fetch(PDO::FETCH_NUM)){
-        echo"<tr>";
-        foreach ($ligne as $valeur){
-            echo "<td>$valeur</td>;
-        }
-        echo "</tr>";
-    }
-}
-    };
-};
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TORILLEC</title>
+    <link rel="stylesheet" type="text/css" href="index.css">
+    <link rel="stylesheet" type="text/css" href="user.css">
+    <link rel="icon" href="images/logo.png" type="image/png">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.13.2/datatables.min.css"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"></head>
+</head>
+
+<body style="background-color: #335686;">
+    <div class="container">
+        <ul>
+            <li><a class="active" href="user.html">TORILLEC COMPANY</a></li>
+            <li style="float:right"><a class="login" href="login.html">Log out</a></li>
+        </ul>
+        <div class="centered-block">
+            <button onclick="togglePopup()">Ajouter un ticket</button>
+        </div>
+        <div id="popup-overlay">
+            <div class="popup-content">
+                <h2 class="mb-4"><b>AJOUTER UN TICKET</b></h2>
+                <div class="mb-3">
+                    <label for="choix" class="form-label">Types de frais*</label>
+                    <select id="types" class="form-select">
+                        <option value="RESTAURATION">RESTAURATION</option>
+                        <option value="LOGEMENT">LOGEMENT</option>
+                        <option value="TRANSPORT">TRANSPORT</option>
+                    </select>
+                </div>
+                <div class="mb-3">
+                    <label for="dateInput" class="form-label">Date*</label>
+                    <input type="date" id="dateInput" class="form-control">
+                </div>
+                <div class="mb-3">
+                    <label for="prixInput" class="form-label">Prix*</label>
+                    <input type="number" id="prixInput" class="form-control">
+                </div>
+                <button id="btnModifier" onclick="submitTicket()" class="btn btn-primary mt-3 me-2">Soumettre</button>
+                <button onclick="togglePopup()" class="btn btn-secondary mt-3">Annuler</button>
+                <a href="javascript:void(0)" onclick="togglePopup()" class="popup-exit">X</a>
+            </div>
+        </div>
+
+        <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">ID</th>
+                <th scope="col">ETAT</th>
+                <th scope="col">TYPE</th>
+                <th scope="col">DATE</th>
+                <th scope="col">PRIX</th>
+                <th scope="col">MODIFICATION</th>
+              </tr>
+            </thead>
+            <tbody id="ticketBody">
+            </tbody>
+        </table>
+    </div>
+    <?php
+    //Connexion db
+    $db = new PDO("mysql:host=localhost;dbname=torillec;charset=utf8mb4","root","");
+    $data = $db->query("SELECT * FROM utilisateur");
+    //Récupération des information de l'utilisateur
+    $id = $_SESSION['id'];
+
+    //Echange avec la base de données
+    $requete = "SELECT * FROM utilisateur";
+    $result = $bddPDO -> query($requete);
+
+    if (!result){
+        echo "La récupération des données a échoué !";
+    }
+    if ($id === $result['id']) {
+        while($ligne = $result->fetch(PDO::FETCH_NUM)) {
+            echo"<tr>";
+            foreach ($ligne as $valeur) {
+                echo "<td>$valeur</td>";
+            }
+            echo "</tr>";
+        };
+    };
+    ?>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.13.2/datatables.min.js"></script>
+    <script src="user.js"></script>
+</body>
+</html>
