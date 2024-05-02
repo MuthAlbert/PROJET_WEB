b1bdb2bf7718dcdfdb318665e5d05200029e8f0b
TomySok <160605249+TomySok@users.noreply.github.com>
2024-05-02 08:29:16 +0200

    Update ajout_ticket.php

--- src/user/ajout_ticket.php
+++ src/user/ajout_ticket.php
@@ -1,1 +1,20 @@
+<?php
+        //Connexion à la base
+        
+        // Récupération des données ajouter à ajouter dans la bdd
+        if($_SERVER["REQUEST_METHOD"] == "POST"){
+            if(isset($_POST['type']) && isset($_POST['etat'] && isset($_POST['somme'] && isset($_POST['date'])){
+        $date = $_POST['date'];
+        $type = $_POST['type'];
+        $somme = $_POST['somme'];
+        $etat = $_POST['etat'];
+        }
+            //Accès a la base
+            $req = $connexion->query("INSERT INTO facture (type, etat, somme, date) VALUES ('$type', '$etat', '$somme', '$date')";
 
+            if ($conn->query($sql) === TRUE) {
+                echo "Nouvelle facture insérée avec succès.";
+            } else {
+                echo "Erreur : " . $sql . "<br>" . $conn->error;
+            }
+?>
