6b4bfc264d628ddba12928421219ab993bed7429
TomySok <160605249+TomySok@users.noreply.github.com>
2024-05-02 11:02:38 +0200

    Update ajout_ticket.php

--- src/user/ajout_ticket.php
+++ src/user/ajout_ticket.php
@@ -1,4 +1,5 @@
-<?php
+<?php   
+session_start();
         //Connexion à la base
         $db = new PDO("mysql:host=localhost;dbname=torillec;charset=utf8mb4","root","");
 
@@ -14,13 +15,15 @@
         $type = $_POST['type'];
         $somme = $_POST['somme'];
         $etat = $_POST['etat'];
+        $id_user = $_SESSION['id'];
         }
             //Accès a la base
-            $req = $db->prepare( query: "INSERT INTO factures (type, etat, somme, date) VALUES (:type, :etat, :somme, :date)");
+            $req = $db->prepare( query: "INSERT INTO factures (date, type, somme, etat, id_user) VALUES (:date, :type, :somme, :etat, :id_user)");
             $req->bindParam(':type', $type);
             $req->bindParam(':etat', $etat);
             $req->bindParam(':somme', $somme);
             $req->bindParam(':date', $date);
+            $req->bindParam(':id_user', $id_user);
 
             if ($req->execute()) {
                 echo "Nouvelle facture insérée avec succès.";
