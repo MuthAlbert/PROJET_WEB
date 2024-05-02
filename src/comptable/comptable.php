a700c2bacb31d023b167a22eb8395369f2215d83
TomySok <160605249+TomySok@users.noreply.github.com>
2024-05-02 11:07:54 +0200

    Update comptable.php

--- src/comptable/comptable.php
+++ src/comptable/comptable.php
@@ -15,7 +15,7 @@ session_start();
     <link rel="stylesheet" href="comptable.css">
 </head>
 <body>
-  
+    <li style="float:right"><a class="login" href="deconnexion.php"><button>Log out</button></a></li>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
 
