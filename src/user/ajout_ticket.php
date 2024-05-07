f96a63e477664cc65c365546cd239441f5519cb7
TomySok <160605249+TomySok@users.noreply.github.com>
2024-05-07 13:17:05 +0200

    Update ajout_ticket.php
    Ajout d'une colonne "nom_utilisateur" dans la table factures

--- src/user/ajout_ticket.php
+++ src/user/ajout_ticket.php
@@ -1,5 +1,7 @@
 <?php   
 session_start();
+
+
         //Connexion à la base
         $db = new PDO("mysql:host=localhost;dbname=torillec;charset=utf8mb4","root","");
 
@@ -16,14 +18,16 @@ session_start();
         $somme = $_POST['somme'];
         $etat = $_POST['etat'];
         $id_user = $_SESSION['id'];
+        $nom_utilisateur = $_SESSION['nom_utilisateur'];
         }
             //Accès a la base
-            $req = $db->prepare( query: "INSERT INTO factures (date, type, somme, etat, id_user) VALUES (:date, :type, :somme, :etat, :id_user)");
+            $req = $db->prepare(query: "INSERT INTO factures (date, type, somme, etat, id_user, nom_utilisateur) VALUES (:date, :type, :somme, :etat, :id_user, :nom_utilisateur)");
             $req->bindParam(':type', $type);
             $req->bindParam(':etat', $etat);
             $req->bindParam(':somme', $somme);
             $req->bindParam(':date', $date);
             $req->bindParam(':id_user', $id_user);
+            $req->bindParam(':nom_utilisateur', $nom_utilisateur);
 
             if ($req->execute()) {
                 echo "Nouvelle facture insérée avec succès.";
