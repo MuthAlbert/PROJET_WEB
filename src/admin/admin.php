739ba0ceb1d596185fde6018bdc8e7998137ab70
Yhernam <116239145+Yhernam@users.noreply.github.com>
2024-05-12 21:04:16 +0200

    Update admin.php
    Nettoyage + pagination + recherche barre

--- src/admin/admin.php
+++ src/admin/admin.php
@@ -15,6 +15,11 @@ if(isset($_SESSION['logAdmin']) != true || $_SESSION['logAdmin'] != true) {
     <title>Administration</title>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap5.min.css" />
+    <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+  <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+  <script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script>
+
     <link rel="stylesheet" href="admin.css">
     <script  src="admin_js.js"></script>
 </head>
@@ -36,71 +41,103 @@ if(isset($_SESSION['logAdmin']) != true || $_SESSION['logAdmin'] != true) {
         </nav>
       
 
-        <h1>Bienvenue !</h1>
+        <div class="col-md-12 text-center">
+            <h1>Administrateur </h1>
+        </div>
 
 
     <!--  On ajoute un utilisateur ici -->
   <center>
-  <div class="cadre2">
-  <h1 class="ajout"> Ajouter un utilisateur</h1><br>
-  <form class="marge"action="admin.php" method="post">
-    <h2 class="texte">Nom</h2>
-    <input type="text" id="nom" name="nom" class="form-control"><br>
-    <h2 class="texte">Prénom</h2>
-    <input type="text" id="prenom" name="prenom" class="form-control"><br>
-    <h2 class="texte">Mail</h2>
-    <input  type="text" id="mail" name="mail" class="form-control"><br>
-    <h2 class="texte">Mot de passe</h2>
-    <input type="password" id="mot_de_passe" name="mot_de_passe" class="form-control"><br>
-    <h2 class="texte">Choisir un rôle</h2>
-    <select class="form-control texte" name="role" id="role">
-      <option value="1">Admin</option>
-      <option value="2">Comptable</option>
-      <option value="3">Commerciale</option>
-    </select>
+    <div class="cadre2">
+      <h1 class="ajout"> Ajouter un utilisateur</h1><br>
+
+      <form class="marge"action="admin.php" method="post">
+
+        <h2 class="texte">Nom</h2>
+        <input type="text"
+                id="nom"
+                name="nom"
+                class="form-control"
+        ><br>
+
+        <h2 class="texte">Prénom</h2>
+        <input type="text"
+                id="prenom"
+                name="prenom"
+                class="form-control"
+        ><br>
+
+        <h2 class="texte">Mail</h2>
+        <input  type="text"
+                id="mail"
+                name="mail" 
+                class="form-control"
+        ><br>
+
+        <h2 class="texte">Mot de passe</h2>
+        <input type="password"
+                id="mot_de_passe"
+                name="mot_de_passe"
+                class="form-control"
+        ><br>
+        <h2 class="texte">Choisir un rôle</h2>
+
+        <select class="form-control texte"name="role" id="role">
+          <option value="1">Admin</option>
+          <option value="2">Comptable</option>
+          <option value="3">Commerciale</option>
+        </select>
 
 
-<?php
-      // Connexion à la base de données
-      $db = new PDO("mysql:host=localhost;dbname=torillec;charset=utf8mb4","root","");
+          <?php
+                  // Connexion à la base de données
+                  $db = new PDO("mysql:host=localhost;dbname=torillec;charset=utf8mb4","root","");
 
-      if ($_SERVER["REQUEST_METHOD"] === "POST"){
-        if (isset($_POST['nom']) 
-            && isset($_POST['prenom']) 
-            && isset($_POST['mail']) 
-            && isset($_POST['mot_de_passe']) 
-            && isset($_POST['role'])) {
+                if ($_SERVER["REQUEST_METHOD"] === "POST"){
+                  if (isset($_POST['nom']) 
+                    && isset($_POST['prenom']) 
+                    && isset($_POST['mail']) 
+                    && isset($_POST['mot_de_passe']) 
+                    && isset($_POST['role'])) {
 
-              $nom = $_POST['nom'];
-              $prenom = $_POST['prenom'];
-              $mail = $_POST['mail']; 
-              $mot_de_passe = $_POST['mot_de_passe'];
-              $role = $_POST['role']; 
+                        $nom = $_POST['nom'];
+                        $prenom = $_POST['prenom'];
+                        $mail = $_POST['mail']; 
+                        $mot_de_passe = $_POST['mot_de_passe'];
+                        $role = $_POST['role']; 
               
-              $stmt=$db->prepare("INSERT INTO utilisateur (prenom,nom,role,mot_de_passe,mail) VALUES(:prenom,:nom,:role,:mot_de_passe,:mail)");
-              $stmt->bindParam(":prenom",$prenom);
-              $stmt->bindParam(":nom",$nom);
-              $stmt->bindParam(":role",$role);
-              $stmt->bindParam(":mot_de_passe",$mot_de_passe);
-              $stmt->bindParam(":mail",$mail);
-              $stmt->execute();
-              header("Location: admin.php");
-              exit();
-        }}    
-    ?>
-    <br>
-    <input class="bouton_ajt" type="submit" value="Ajouter l'utilisateur">
-    <input class="bouton_initialiser" type="reset" value="Effacer">
-    <br>
-    </div>
-  </form>
+                        $stmt=$db->prepare("INSERT INTO utilisateur (prenom,nom,role,mot_de_passe,mail) VALUES(:prenom,:nom,:role,:mot_de_passe,:mail)");
+                        $stmt->bindParam(":prenom",$prenom);
+                        $stmt->bindParam(":nom",$nom);
+                        $stmt->bindParam(":role",$role);
+                        $stmt->bindParam(":mot_de_passe",$mot_de_passe);
+                        $stmt->bindParam(":mail",$mail);
+                        $stmt->execute();
+                        header("Location: admin.php");
+                        exit();
+                    }
+                  }    
+            ?>
 
 
+    <br>
+    <input class="bouton_ajt"
+            type="submit"
+            value="Ajouter l'utilisateur"
+    >
+    <input class="bouton_initialiser"
+            type="reset"
+            value="Effacer"
+    >
+    <br>
+  </div>
+</form>
 
 <!---afficher les données de tout les users, supprimer les users--->
-<table class="table cadre" > <!-- Affichage dans un tableau -->
+<div class="cadre2">
+  <table id="myTable" class="table cadre" > <!-- Affichage dans un tableau -->
     <thead>
-    <tr>
+      <tr>
         <th scope="col">Id utilisateur</th>
         <th scope="col">Nom</th>
         <th scope="col">Prénom</th>
@@ -108,58 +145,82 @@ if(isset($_SESSION['logAdmin']) != true || $_SESSION['logAdmin'] != true) {
         <th scope="col">Mail</th>
         <th scope="col">Mot de Passe</th>
         <th scope="col">Actions</th>
-    </tr>
+      </tr>
     </thead>
     <tbody>
 
- <?php
-   if (isset($_GET['id']) && isset($_GET['action']) && $_GET['action']=='delete'){
-     $id=$_GET['id'];
-     $suppr = "Utilisateur supprimé";
-     $db = new PDO("mysql:host=localhost;dbname=torillec;charset=utf8mb4","root","");
-     $stmt=$db->prepare("DELETE FROM utilisateur WHERE id_user=:id");
-     $stmt->bindParam (":id",$id);
-     $req = $db->prepare("UPDATE factures SET nom_utilisateur = CONCAT(nom_utilisateur, :suppr) WHERE id_user = :id");
-     $req->execute(['suppr' => $suppr, 'id' => id]);
+    
+
+
+          <?php
+          if (isset($_GET['id']) && isset($_GET['action']) && $_GET['action']=='delete'){
+            $id=$_GET['id'];
+            $suppr = "Utilisateur supprimé";
+            $db = new PDO("mysql:host=localhost;dbname=torillec;charset=utf8mb4","root","");
+            $stmt=$db->prepare("DELETE FROM utilisateur WHERE id_user=:id");
+            $stmt->bindParam (":id",$id);
+            $req = $db->prepare("UPDATE factures SET nom_utilisateur = CONCAT(nom_utilisateur, :suppr) WHERE id_user = :id");
+            $req->execute(['suppr' => $suppr, 'id' => $id]);
      
-     $stmt->execute();
-     header("Location: admin.php");
-     exit();
+            $stmt->execute();
+            header("Location: admin.php");
+            exit();
 
-   }     
-   $db = new PDO("mysql:host=localhost;dbname=torillec;charset=utf8mb4","root","");
-   $data = $db->query("SELECT * FROM utilisateur")->fetchAll();
+          }     
+          $db = new PDO("mysql:host=localhost;dbname=torillec;charset=utf8mb4","root","");
+          $data = $db->query("SELECT * FROM utilisateur")->fetchAll();
 
-    foreach ($data as $row){
-      echo "<tr>";
-      echo "<td>".$row["id_user"]."</td>";
-      echo "<td>".$row["nom"]."</td>";
-      echo "<td>".$row["prenom"]."</td>";
-      echo "<td>".$row["role"]."</td>";
-      echo "<td>".$row["mail"]."</td>"; 
-      echo "<td>".$row["mot_de_passe"]."</td>";
-      echo '<td>
-            <div class="dropdown">
-                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
-                    Actions
-                </button>
-                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                    <li><a class="dropdown-item bouton_modifier" href="edit_user.php?id='.$row['id_user'].'">Modifier</a></li>
-                    <li><a class="dropdown-item bouton_supprimer" href="#" onclick="confirmDelete('.$row['id_user'].')">Supprimer</a></li>
-                </ul>
-            </div>
-        </td>';
-    echo "</tr>";
+          foreach ($data as $row){
+            echo "<tr>";
+            echo "<td>".$row["id_user"]."</td>";
+            echo "<td>".$row["nom"]."</td>";
+            echo "<td>".$row["prenom"]."</td>";
+            echo "<td>".$row["role"]."</td>";
+            echo "<td>".$row["mail"]."</td>"; 
+            echo "<td>".$row["mot_de_passe"]."</td>";
+            echo '<td>
+                    <div class="dropdown">
+                        <button class="btn btn-secondary dropdown-toggle"
+                                type="button"
+                                id="dropdownMenuButton" 
+                                data-bs-toggle="dropdown" 
+                                aria-expanded="false"
+                                >Actions
+                        </button>
+                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <li>
+                              <a class="dropdown-item bouton_modifier"
+                                  href="edit_user.php?id='.$row['id_user'].'"
+                                  >Modifier
+                              </a>
+                            </li>
+                            <li>
+                              <a class="dropdown-item bouton_supprimer" 
+                                  href="#" 
+                                  onclick="confirmDelete('.$row['id_user'].')"
+                                  >Supprimer
+                              </a>
+                            </li>
+                        </ul>
+                      </div>
+                  </td>';
+            echo "</tr>";
 
       }
  
  ?>
 
-    </div>
-</div>
-</tbody>
-</body>
+        </div>
+      </div>
+    </tbody>
+  </body>
 </table>
-</div>
-    </div>
+
+<!-- SCRIPT POUR DATATABLE -->
+    <script>
+    $(document).ready( function () {
+      $('#myTable').DataTable();
+      });
+    </script>
+</html>
 </html>
