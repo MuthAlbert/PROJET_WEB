e21bbe8db959ea00640d1259371bcdf81e561384
Yhernam <116239145+Yhernam@users.noreply.github.com>
2024-05-14 18:46:27 +0200

    Update comptable.php

--- src/comptable/comptable.php
+++ src/comptable/comptable.php
@@ -14,6 +14,7 @@ if(isset($_SESSION['logComptable']) != true || $_SESSION['logComptable'] != true
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TORILLEC - Comptable</title>
     <link rel="stylesheet" type="text/css" href="../../index.css">
+    <link rel="stylesheet" type="text/css" href="comptable.css">
     <link rel="icon" href="../../Images/logo.png" type="image/png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
